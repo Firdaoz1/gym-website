094754e506a2ce6968c4774504d5e7853460aa16
Firdaoz <bmayowa868@gmail.com>
2026-09-15 16:31:02 +0100

    index.html, style.css, index.js formatted

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const menuToggle = document.querySelector("#mobile-btn");
 const menuBack = document.querySelector("#mobile-back");
 const mobileMenu = document.querySelector("#mobile-links");
 
-
 menuToggle.addEventListener("click", () => {
   mobileMenu.classList.add("active");
 });
-
 
 menuBack.addEventListener("click", () => {
   mobileMenu.classList.remove("active");
